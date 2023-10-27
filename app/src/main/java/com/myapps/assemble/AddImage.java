be5238db62430cbba7066aa4ed8d096243ff4930
Praveen <pattarpraveen15@gmail.com>
2023-10-27 13:20:21 +0530

    Screen3

--- app/src/main/java/com/myapps/assemble/AddImage.java
+++ app/src/main/java/com/myapps/assemble/AddImage.java
@@ -2,15 +2,35 @@ package com.myapps.assemble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddImage extends AppCompatActivity {
 
+    RelativeLayout relativeLayout;
     ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9;
     ImageView[] imageViews = new ImageView[]{};
+
+    Button nxtButton;
 
     private ImageView selectedImage;
     private int offsetX, offsetY;
@@ -19,6 +39,9 @@ public class AddImage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_image);
+
+        nxtButton = this.findViewById(R.id.nxtButton);
+        relativeLayout = this.findViewById(R.id.RLayout);
 
         img1 = findViewById(R.id.imagePart1);
         img2 = findViewById(R.id.imagePart2);
@@ -35,12 +58,15 @@ public class AddImage extends AppCompatActivity {
         boolean[] isImageVisible = getIntent().getBooleanArrayExtra("imageVisibility");
 
         for (int i = 0; i < imageViews.length; i++) {
+            assert isImageVisible != null;
             if (isImageVisible[i]) {
                 imageViews[i].setVisibility(View.VISIBLE);
             } else {
                 imageViews[i].setVisibility(View.GONE);
             }
         }
+
+        List<Pair<Float, Float>> imagePositions = new ArrayList<>();
 
         View.OnTouchListener onTouchListener = new View.OnTouchListener() {
             @Override
@@ -63,6 +89,10 @@ public class AddImage extends AppCompatActivity {
                         selectedImage = null;
                         break;
                 }
+
+                // Add the position to the list
+                /*imagePositions.add(new Pair<>(selectedImage.getX(), selectedImage.getY()));*/
+
                 return true;
             }
         };
@@ -77,5 +107,77 @@ public class AddImage extends AppCompatActivity {
         img8.setOnTouchListener(onTouchListener);
         img9.setOnTouchListener(onTouchListener);
 
+// Pass the list to the next activity
+        nxtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddImage.this, Choose_Color.class);
+
+                Bitmap bitmap = getBitmapFromView(relativeLayout);
+                saveBitmapToDownloads(getApplicationContext(),bitmap,"my_image");
+                String filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/my_image.png";
+                intent.putExtra("imagePath", filePath);
+                //intent.putExtra("imagePositions", new ArrayList<>(imagePositions));
+                startActivity(intent);
+            }
+        });
+
     }
+
+    private Bitmap getBitmapFromView(View view) {
+        //Define Bitmap of same size
+        Bitmap returnBitmap = Bitmap.createBitmap(view.getWidth(),view.getHeight(),Bitmap.Config.ARGB_8888);
+        //Bind a canvas to it
+        Canvas canvas = new Canvas(returnBitmap);
+        //Get the background view of layout
+        Drawable background = view.getBackground();
+        if (background != null){
+            background.draw(canvas);
+        }else {
+            canvas.drawColor(Color.WHITE);
+        }
+        //drawing view on canvas
+        view.draw(canvas);
+
+        return returnBitmap;
+    }
+
+    private void saveBitmapToDownloads(Context context, Bitmap bitmap, String fileName) {
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+        File file = new File(path, fileName + ".png");
+        FileOutputStream out;
+        try {
+            out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+            out.flush();
+            out.close();
+            // MediaScannerConnection is used to scan the file and update media store
+            MediaScannerConnection.scanFile(context, new String[]{file.toString()}, null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /*private void saveLayoutAsImage(RelativeLayout relativeLayout, String fileName) {
+            relativeLayout.setDrawingCacheEnabled(true);
+            relativeLayout.buildDrawingCache();
+            Bitmap bitmap = Bitmap.createBitmap(relativeLayout.getDrawingCache());
+            relativeLayout.setDrawingCacheEnabled(false);
+
+            File file = new File(Environment.getExternalStorageDirectory() + File.separator + fileName + ".png");
+            FileOutputStream outputStream = null;
+            try {
+                outputStream = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                outputStream.flush();
+                outputStream.close();
+                // Add the image to the system gallery
+                MediaScannerConnection.scanFile(this, new String[]{file.getAbsolutePath()}, null, null);
+                Toast.makeText(this, "Image Saved", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }*/
+
 }
--- app/src/main/java/com/myapps/assemble/Choose_Color.java
+++ app/src/main/java/com/myapps/assemble/Choose_Color.java
@@ -2,13 +2,44 @@ package com.myapps.assemble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Pair;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class Choose_Color extends AppCompatActivity {
+
+    //ImageView image1,image2,image3,image4,image5,image6,image7,image8,image9;
+    ImageView completeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_color);
+
+        completeImage = this.findViewById(R.id.completeImage);
+        String imagePath = getIntent().getStringExtra("imagePath");
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+        completeImage.setImageBitmap(bitmap);
+        /*image1 = this.findViewById(R.id.image1);
+        image2 = this.findViewById(R.id.image2);
+        image3 = this.findViewById(R.id.image3);
+        image4 = this.findViewById(R.id.image4);
+        image5 = this.findViewById(R.id.image5);
+        image6 = this.findViewById(R.id.image6);
+        image7 = this.findViewById(R.id.image7);
+        image8 = this.findViewById(R.id.image8);
+        image9 = this.findViewById(R.id.image9);*/
+
+        /*ArrayList<Pair<Float, Float>> imagePositions = (ArrayList<Pair<Float, Float>>) getIntent().getSerializableExtra("imagePositions");
+        for (Pair<Float, Float> position : imagePositions) {
+            float xCoordinate = position.first;
+            float yCoordinate = position.second;
+            // Do something with the coordinates
+        }*/
+
     }
 }
